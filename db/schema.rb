8f2d33e5145c51847917a4bfb56dce833e6bea41
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-31 11:04:38 +0800

    [Layout] User Edit (#7)
    * [Layout] User Edit
    
    * fixed layout

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_040523) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "fullname"
     t.string "email", default: "", null: false
+    t.string "phone_number"
+    t.text "description"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
