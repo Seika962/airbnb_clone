15c31321109c7419b4057adbd822918143a959b7
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-31 12:37:26 +0800

    [Layout] User Show Page (#8)
    * [Layout] User Show Page
    
    * fixed indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  resources :users, only: [:show]
   devise_for :users,
               path: '',
               controllers: { registrations: 'registrations'}
