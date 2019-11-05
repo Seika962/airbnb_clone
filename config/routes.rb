cae036f59e91e43ace75649d069e6cd531055e0d
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-11-05 12:03:46 +0800

    [Function] Facebook Authentication (#11)
    * [Function] Facebook Authentication
    
    * fixed indentation
    
    * fixed indentation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   devise_for :users,
               path: '',
-              controllers: { registrations: 'registrations'}
+              controllers: { registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
