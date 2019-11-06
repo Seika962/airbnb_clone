d15c12ca6e860f3ed3192819b3a13fb49e125e34
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-11-06 11:09:24 +0800

    [Function] Create Room (#13)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,23 @@ class RoomsController < ApplicationController
   def new
     @room = current_user.rooms.new
   end
+
+  def create
+    @room = current_user.rooms.new(room_params)
+    if @room.save
+      redirect_to listing_room_url(current_user)
+    else
+      flash[:alert] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def listing
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:home_type,:room_type,:guest_count,:bedroom_count,:bathroom_count)
+  end
+
 end
