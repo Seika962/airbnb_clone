cf74085f57dbe4229d70677ded65c0bc9cf851cb
Seika962 <Seika0622@gmail.com>
2019-11-07 10:42:27 +0800

    added before_action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+  before_action :set_rooms, only: [:listing, :pricing, :description, :photos, :amenities, :location]
+
   def new
     @room = current_user.rooms.new
   end
@@ -13,33 +15,12 @@ class RoomsController < ApplicationController
     end
   end
 
-  def listing
-    @room = current_user.rooms.find(params[:id])
-  end
-
-  def pricing
-    @room = Room.find(params[:id])
-  end
-
-  def description
-    @room = Room.find(params[:id])
-  end
-
-  def photos
-    @room = Room.find(params[:id])
-  end
-
-  def amenities
-    @room = Room.find(params[:id])
-  end
-
-  def location
-    @room = Room.find(params[:id])
-  end
-
   private
   def room_params
     params.require(:room).permit(:home_type,:room_type,:guest_count,:bedroom_count,:bathroom_count)
   end
 
+  def set_rooms
+    @room = Room.find(params[:id])
+  end
 end
