cffb750d276e3f34ca6f3f65fe9edb26a5233028
Seika962 <Seika0622@gmail.com>
2019-11-07 10:28:30 +0800

    [Layout] New Room Pages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,6 +17,26 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.find(params[:id])
   end
 
+  def pricing
+    @room = Room.find(params[:id])
+  end
+
+  def description
+    @room = Room.find(params[:id])
+  end
+
+  def photos
+    @room = Room.find(params[:id])
+  end
+
+  def amenities
+    @room = Room.find(params[:id])
+  end
+
+  def location
+    @room = Room.find(params[:id])
+  end
+
   private
   def room_params
     params.require(:room).permit(:home_type,:room_type,:guest_count,:bedroom_count,:bathroom_count)
