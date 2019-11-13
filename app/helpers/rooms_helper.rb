732a657d756360c34d004ca404a0334402a15fba
Seika962 <Seika0622@gmail.com>
2019-11-13 11:53:36 +0800

    [Function] Publish Room

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,11 @@
 module RoomsHelper
+  def is_done?(form)
+    unless form.blank?
+      content_tag :i, "", class:"fa fa-check float-right pb-2 text-dark"
+    end
+  end
+
+  def is_ready?(room)
+    !room.price.blank? && !room.listing_name.blank? && !room.description.blank? && !room.photos.blank? && !room.address.blank?
+  end
 end
