4d24b6a7815bad1e29a482d971968068a1c38756
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-11-19 09:47:35 +0800

    [Function] Google Map (#23)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_021138) do
+ActiveRecord::Schema.define(version: 2019_11_18_044036) do
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_11_08_021138) do
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longtitude"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
