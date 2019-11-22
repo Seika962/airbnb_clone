fb6fa59da8684b4ac8dec931c0c48be678e4cb88
Seika962 <Seika0622@gmail.com>
2019-11-22 11:22:28 +0800

    [Function] Review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :rooms
   has_many :reservations
+  has_many :guest_reviews, class_name: "GuestReview", foreign_key: "guest_id"
+  has_many :host_reviews, class_name: "HostReview", foreign_key: "host_id"
   validates :fullname, presence: true, length: { maximum: 50 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
